e0f05f55b42c4876e02749555c78db92d39a1e00
kofikoomson <kofikoomson94@gmail.com>
2023-03-17 18:51:41 -0700

    if else c program using betty styling

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,36 +1,23 @@
 #include <stdlib.h>
-
 #include <time.h>
-
 #include <stdio.h>
 
-
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
-/* main function determines if the number is negative positive or zero*/
+/**
+ * main - assigns a random number to int n everytime
+ * it executes, and prints it
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	
 	if (n > 0)
-	{
-	printf("%d is positive\n", n);
-	}
+		printf("%d is positive\n", n);
 	else if (n == 0)
-	{
-        printf("%d is zero\n", n);
-       	}
+		printf("%d is zero\n", n);
 	else if (n < 0)
-	{
-        printf("%d is negative\n", n);
-        }
-	else
-	printf("the number you entered is invalid, please try again");
+		printf("%d is negative\n", n);
 	return (0);
 }
