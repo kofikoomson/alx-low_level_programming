de92fa768dfd60989974968051cf16e3812f8482
kofikoomson <kofikoomson94@gmail.com>
2023-03-21 12:02:09 -0700

    10-add.c add up

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - function adds two integers and returns the result
+ * @number1: first integer 
+ * @number2: second integer
+ * Return: @result
+ */
+int add(int number1, int number2)
+{
+	int result;
+
+	result = number1 + number2;
+
+	return (result);
+}
