9d81e372593e1fbce1147e83a69eee1b6ddc3de5
kofikoomson <kofikoomson94@gmail.com>
2023-03-21 09:11:55 -0700

    3-islower.c add up fix

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,7 +2,7 @@
 
 /**
  *function  _islower - Checks for lowercase character.
- * if else loop to check for character 'c'
+ * if else loop to check for character : c
  * Return: 1 if character is lowercase, 0 otherwise.
  */
 int _islower(int c)
