cd9d7c1698c6ba4e4610da5034b0cde0fab5c48a
kofikoomson <kofikoomson94@gmail.com>
2023-04-17 09:06:50 -0700

    dog.h fix

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,11 @@
-ifndef DOG_H
+#ifndef DOG_H
 #define DOG_H
 
 /**
  * struct dog - a dog's basic info
- * @name: First member
- * @age: Second member
- * @owner: Third member
+ * @name: First element
+ * @age: Second element
+ * @owner: Third element
  *
  */
 struct dog
