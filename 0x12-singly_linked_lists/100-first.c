42fd0ade547963c9069c944fc8d4dbea2c7bbee0
kofikoomson <kofikoomson94@gmail.com>
2023-05-01 02:36:18 -0700

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,18 +1,13 @@
-#include<stdio.h>
+#include <stdio.h>
+
+void __attribute__((constructor)) tortoise(void);
 
 /**
- * myStartupFun - Apply the constructor attribute to myStartupFun() so that it
- *   is executed before main()
+ * tortoise - Prints a string before the
+ *        main function is executed.
  */
-
-void myStartupFun(void) __attribute__ ((constructor));
-
-/**
- * myStartupFun - implementation of myStartupFun
- */
-
-void myStartupFun(void)
+void tortoise(void)
 {
-	printf("You're beat! and yet, you must allow,\n");
-	printf("I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n"
+	       "I bore my house upon my back!\n");
 }
