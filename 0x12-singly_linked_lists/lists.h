b94d4b1e4b9f34646b789a45c49a1d1a1763c84f
kofikoomson <kofikoomson94@gmail.com>
2023-07-06 10:19:08 -0700

    add a node for me

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -6,6 +6,7 @@
  * @str: string - (malloc'ed string)
  * @len: length of the string
  * @next: points to the next node
+ *
  * Description: singly linked list node structure
  */
 typedef struct list_s
@@ -17,6 +18,7 @@ typedef struct list_s
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
 
 #endif
 
