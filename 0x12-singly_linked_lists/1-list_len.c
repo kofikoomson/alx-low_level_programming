7195a03cd1de30a83258847aa94dd6a47bfced57
kofikoomson <kofikoomson94@gmail.com>
2023-07-06 10:14:09 -0700

    list length

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h> 
+#include "lists.h"
+
+/**
+ * list_len - check the code for Holberton School students.
+ * @h: name of the list
+ * Return: the no of elements.
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t element = 0;
+
+	while (h)
+	{
+		element++;
+		h = h->next;
+	}
+
+	return (element);
+}
+
