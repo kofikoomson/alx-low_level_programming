da257591b14c096670838d13dced0dd6d15e362c
kofikoomson <kofikoomson94@gmail.com>
2023-07-11 05:29:43 -0700

    copy the content

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -12,5 +12,9 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+char *create_buffer(char *file);
+void close_file(int fd);
+int _putchar(char c);
+int main(int argc, char *argv[])
 
 #endif
